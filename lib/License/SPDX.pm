9df397932c191fbecb25b529a5867e902acaa3de
Jonathan Stowe <jns@gellyfish.co.uk>
2019-02-20 12:11:38 +0000

    kebabify

--- lib/License/SPDX.pm
+++ lib/License/SPDX.pm
@@ -3,18 +3,18 @@ use JSON::Name;
 use JSON::Class;
 class License::SPDX does JSON::Class {
     class License does JSON::Class {
-        has Str $.name;
-        has Bool $.isDeprecatedLicenseId;
-        has Bool $.isOsiApproved;
-        has Str $.licenseId;
-        has Str $.reference;
-        has Str $.detailsUrl;
-        has  @.seeAlso;
-        has Str $.referenceNumber;
-        has Bool $.isFsfLibre;
+        has Str     $.name;
+        has Bool    $.is-deprecated-license-id  is json-name('isDeprecatedLicenseId');
+        has Bool    $.is-osi-approved           is json-name('isOsiApproved');
+        has Str     $.license-id                is json-name('licenseId');
+        has Str     $.reference;
+        has Str     $.details-url               is json-name('detailsUrl');
+        has Str     @.see-also                  is json-name('seeAlso');
+        has Str     $.reference-snumber         is json-name('referenceNumber');
+        has Bool    $.is-fsf-libre              is json-name('isFsfLibre');
     }
-    has Str $.licenseListVersion;
-    has Str $.releaseDate;
+    has Str     $.license-list-version  is json-name('licenseListVersion');
+    has Str     $.release-date          is json-name('releaseDate');
     has License @.licenses;
 }
 
